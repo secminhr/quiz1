691d2562c6315258ff5e8af0c2428377756213f6
secminhr <secminhrian@gmail.com>
2021-03-07 14:33:14 +0800

    Non-recursive quicksort
    Use loop and arrays to imitate recursion and stack.
    Control jump behavior with boolean flags.

--- list.c
+++ list.c
@@ -1,5 +1,6 @@
 #include "list.h"
 #include <stdlib.h>
+#include <stdbool.h>
 
 static inline void list_add_node_t(node_t **list, node_t *node_t)
 {
@@ -17,27 +18,62 @@ static inline void list_concat(node_t **left, node_t *right)
 
 void quicksort(node_t **list)
 {
-    if (!*list)
-        return;
+    #define MAX_LEVELS 1000
+    node_t **list_arr[MAX_LEVELS] = { list };
+    node_t *l_arr[MAX_LEVELS], *r_arr[MAX_LEVELS], *piv_arr[MAX_LEVELS];
+    bool left_sorted[MAX_LEVELS], right_sorted[MAX_LEVELS];
+    int i = 0;
+    while (i >= 0) {
+        if (*list_arr[i]) {
+            if (left_sorted[i]) {
+                if (right_sorted[i]) {
+                    goto CONCAT;
+                } else {
+                    goto SORT_RIGHT;
+                }
+            }
+            PAIRTITION: {
+                node_t *left = NULL, *right = NULL, *pivot = *list_arr[i];
+                int value = pivot->value;
+                node_t *p = pivot->next;
+                pivot->next = NULL;
 
-    node_t *pivot = *list;
-    int value = pivot->value;
-    node_t *p = pivot->next;
-    pivot->next = NULL;
-
-    node_t *left = NULL, *right = NULL;
-    while (p) {
-        node_t *n = p;
-        p = p->next;
-        list_add_node_t(n->value > value ? &right : &left, n);
+                while (p) {
+                    node_t *n = p;
+                    p = p->next;
+                    list_add_node_t(n->value > value ? &right : &left, n);
+                }
+                l_arr[i] = left;
+                r_arr[i] = right;
+                piv_arr[i] = pivot;
+            }
+            SORT_LEFT: {
+            	left_sorted[i] = true;
+            	left_sorted[i+1] = false;
+                right_sorted[i+1] = false;
+                list_arr[i+1] = &l_arr[i];
+                i++;
+                continue;
+            }
+            SORT_RIGHT: {
+                right_sorted[i] = true;
+                left_sorted[i+1] = false;
+                right_sorted[i+1] = false;
+                list_arr[i+1] = &r_arr[i];
+                i++;
+                continue;
+            }
+            CONCAT: {
+                node_t *result = NULL;
+                list_concat(&result, l_arr[i]);
+                list_concat(&result, piv_arr[i]);
+                list_concat(&result, r_arr[i]);
+                *list_arr[i] = result;
+                i--;
+            }
+        } else {
+            i--;
+        }
     }
-
-    quicksort(&left);
-    quicksort(&right);
-
-    node_t *result = NULL;
-    list_concat(&result, left);
-    list_concat(&result, pivot);
-    list_concat(&result, right);
-    *list = result;
+    *list = *list_arr[0];
 }
